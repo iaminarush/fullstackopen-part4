7704bf5952d20296b58f911078883adc16b4e901
pat <patrickcheng1995@gmail.com>
2022-02-08 01:34:55 +0800

    4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,6 +11,12 @@ test("blogs are returned as json", async () => {
     .expect("Content-type", /application\/json/);
 }, 100000);
 
+test("id", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body[0].id).toBeDefined();
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
